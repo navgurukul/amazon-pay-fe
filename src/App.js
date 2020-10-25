673afd809657683fcc220dda67742b6c6e3ce90e
pysaquib <iamsaquibnasim@gmail.com>
2020-10-25 20:12:56 +0530

    some changes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from "react";
 import "./App.css";
 import Header from "./Componets/Header";
-import Contant from "./Componets/Contant";
+import Content from "./Componets/Content";
 import Form from "./Componets/Form";
 import Footer from "./Componets/Footer";
 function App() {
   return (
     <div className="App">
       <Header />
-      <Contant />
+      <Content />
       <Form />
       <Footer />
     </div>
