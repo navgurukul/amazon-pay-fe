75a66425808ea4289448167b4fc4ca5185f6dfa4
komalahire <komala18@navgurukul.org>
2020-10-25 15:16:11 +0530

    Have made an amazon pay form

---
+++ src/Componets/Header.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Button from '@material-ui/core/Button';
+import Grid from '@material-ui/core/Grid';
+import Logo from './logo.png'
+import Container from '@material-ui/core/Container';
+
+export default function Header() { 
+  return ( 
+    <div className="appbarwrapper">
+    <AppBar position="fixed">
+    <Container  component="main"  >
+      <Toolbar>
+        <Grid
+          justify="space-between" 
+          container
+        >
+          <Grid item>
+            <a href="#">
+            <Button color="inherit">
+            <img src={Logo}  alt="Logo"  style={{height:'40px'}}/>
+          </Button>
+          </a>
+          </Grid>
+          <Grid item>
+            <div
+            > 
+              <a href="#"><Button color="inherit">Home</Button></a> 
+              <a href="https://navgurukul.org/"> <Button color="inherit">About Us</Button></a>
+              <a href="https://medium.com/navgurukul"> <Button color="inherit">BLOG</Button></a>
+              <a href="https://merakilearn.org/"> <Button color="inherit">MERAKI</Button></a>
+              <a href="#"><Button color="inherit">Contact Us</Button></a>
+            </div>
+          </Grid>
+        </Grid>
+      </Toolbar>
+      </Container>
+    </AppBar>
+    </div>
+  );
+}
--- src/components/Logo/logo.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import LogoImg from "../../assets/logo.png";
-import "./logo.css";
-
-function Logo() {
-  return <img className="logo" src={LogoImg} alt="navgurukul"></img>;
-}
-
-export default Logo;
