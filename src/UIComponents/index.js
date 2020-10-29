d43b7e8c99fcd43bcaba68f715c80450989de8cd
Abhishek Gupta <a@navgurukul.org>
2020-10-29 16:42:26 +0530

    deleted file

--- src/UIComponents/index.js
+++ src/UIComponents/index.js
@@ -1,5 +1,6 @@
 import { Typography } from "@material-ui/core";
 import React from "react";
+import Grid from "@material-ui/core/Grid";
 import KajalPic from "../assets/kajal-ahirwal.png";
 
 const NAME = "Navgurukul";
@@ -26,13 +27,21 @@ function TEXTHEAD() {
 function MAIN_CONTENT() {
   return (
     <>
-      <img src={KajalPic} alt="Kajal Ahirwal" />;
-      <Typography component="p" variant="body1" className="text1">
-        More than 100+ students have been placed into various MNCs and startups as Software Programmers.
-      </Typography>
-      <Typography component="p" variant="body1" className="text1">
-        You can donate heartily to this campaign to support employment of girls from marginalised areas across the country.
-      </Typography>
+      <Grid item xs={12} style={{ marginTop: "8%" }}>
+        <Grid item xs={12} style={{flexDirection: "row", display: "flex", alignItems: "center"}}>
+          <img src={KajalPic} alt="Kajal Ahirwal" style={{height:96}}/>;
+          <Typography component="p" variant="body1" style={{margin: 16, marginTop: 0}}>
+            Kajal an 18 year old girl, a 10th standard student, with less than 6 months of training at Navgurukul - is now working at <b>Mindtree</b> with a 4.2 lacs annual package.  Based out of Delhi, her father is a rickshaw puller and mother a domestic cook in Delhi.
+          </Typography>          
+        </Grid>
+
+        <Typography component="p" variant="body1" className="text1">
+          One-year fully residential course for students from low-income & marginalised communities in Software Engineering enabling them to get an aspirational job, have a voice, and be equipped to bring at least 10 families out of poverty in a financially sustainable model.
+          <br/>
+          <br/>
+          Donate now towards future of 600+ girls like Kajal enroled in our residential courses now.
+        </Typography>
+      </Grid>
     </>
   );
 }
