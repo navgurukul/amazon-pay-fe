368e606fe261e7453109d49d37639f18c8956a8c
pysaquib <iamsaquibnasim@gmail.com>
2020-10-29 14:21:50 +0530

    ok

--- src/Components/Content.js
+++ src/Components/Content.js
@@ -3,7 +3,7 @@ import { makeStyles } from "@material-ui/core/styles";
 import Grid from "@material-ui/core/Grid";
 import Container from "@material-ui/core/Container";
 import Typography from "@material-ui/core/Typography";
-import {TEXTHEAD, TEXT1, TEXT2} from "../UIComponents";
+import { TEXTHEAD, TEXT1, TEXT2 } from "../UIComponents";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -25,17 +25,12 @@ export default function Text() {
       <Grid container spacing={3}>
         <Grid item xs={12} style={{ marginTop: "8%" }}>
           <Container maxWidth="md">
-            <Typography component="h4" variant="h4" className="Heading" className="text-align-center">
-              {TEXTHEAD}
-            </Typography>
-            <Typography component="p" variant="body1" className="text1">
-              {TEXT1}
-            </Typography>
+            <TEXTHEAD />
+            <TEXT1 />
+            <TEXT2 />
             <Typography component="p" variant="body1" className="text2">
-              {TEXT2}
-            </Typography>
-            <Typography component="p" variant="body1" className="text2">
-              <b>Note</b>: Only Indian Citizens making payments through their local bank accounts can donate.
+              <b>Note</b>: Only Indian Citizens making payments through their
+              local bank accounts can donate.
             </Typography>
           </Container>
         </Grid>
--- src/UIComponents.js
+++ src/UIComponents.js
@@ -1,19 +1,48 @@
-const NAME = "Navgurukul";
-const LOGO = "https://navgurukul.org/assets/img/logo.png";
-const HOME_PAGE = "https://navgurukul.org/";
-const RETURN_URL = "https://donate.navgurukul.org/";
-const BLOG = "https://medium.com/navgurukul";
-const TEXT1 = `More than 100+ students have been placed into various MNCs and startups as Software Programmers.`;
-const TEXT2 = `You can donate heartily to this campaign to support employment of girls from marginalised areas across the country.`;
-const TEXTHEAD = `Help girls from Low-Income Communities to become Software Engineers and uplift more women`;
+import { Typography } from "@material-ui/core";
+import React from "react";
 
-module.exports = {
-  NAME,
-  LOGO,
-  HOME_PAGE,
-  RETURN_URL,
-  BLOG,
-  TEXT1,
-  TEXT2,
-  TEXTHEAD,
-};
+const NAME = "Peepul";
+const LOGO = "https://navgurukul.org/assets/img/peepul.png";
+const HOME_PAGE = "https://www.peepulindia.org/";
+const RETURN_URL = "https://donate.peepulschools.org/";
+const BLOG = "https://twitter.com/peepulindia?lang=en";
+
+function TEXTHEAD() {
+  return (
+    <Typography
+      component="h4"
+      variant="h4"
+      className="Heading"
+      className="text-align-center"
+    >
+      Donate for devices.
+      <br />
+      Make a difference.
+    </Typography>
+  );
+}
+
+function TEXT1() {
+  return (
+    <Typography component="p" variant="body1" className="text1">
+      At Peepul, we look to improve learning outcomes in the public education
+      system, by enabling ‘meaningful teacher-student interaction’. If a child
+      is meaningfully and effectively engaged in the classroom by the teacher,
+      she attends classes regularly, engages in learning, and in turn, achieves
+      learning.
+    </Typography>
+  );
+}
+
+function TEXT2() {
+  return (
+    <Typography component="p" variant="body1" className="text1">
+      Towards this, we closely partner with national, state, and local
+      governments to design and implement high-quality, scalable solutions that
+      create these high-performing teachers, through a combination of training,
+      coaching and performance management.
+    </Typography>
+  );
+}
+
+export { NAME, LOGO, HOME_PAGE, RETURN_URL, BLOG, TEXT1, TEXT2, TEXTHEAD };
