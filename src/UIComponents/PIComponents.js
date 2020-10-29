6d839de9db917b9bca9facdbb0e624e586765ad6
pysaquib <iamsaquibnasim@gmail.com>
2020-10-29 15:48:20 +0530

    PI layout changes

--- src/UIComponents/PIComponents.js
+++ src/UIComponents/PIComponents.js
@@ -1,5 +1,7 @@
-import { Typography } from "@material-ui/core";
 import React from "react";
+import { Typography } from "@material-ui/core";
+
+import DonationImage from "../assets/donation.jpg";
 
 const NAME = "Peepul";
 const LOGO = "https://navgurukul.org/assets/img/peepul.png";
@@ -14,6 +16,7 @@ function TEXTHEAD() {
       variant="h4"
       className="Heading"
       className="text-align-center"
+      style={{ fontFamily: "Comfortaa", color: "#0d154a" }}
     >
       Donate for devices.
       <br />
@@ -24,25 +27,58 @@ function TEXTHEAD() {
 
 function TEXT1() {
   return (
-    <Typography component="p" variant="body1" className="text1">
-      At Peepul, we look to improve learning outcomes in the public education
-      system, by enabling ‘meaningful teacher-student interaction’. If a child
-      is meaningfully and effectively engaged in the classroom by the teacher,
-      she attends classes regularly, engages in learning, and in turn, achieves
-      learning.
+    <Typography
+      style={{ fontFamily: "Open Sans", fontWeight: 600 }}
+      component="p"
+      variant="body1"
+      className="text1"
+    >
+      Thank you for your support! We support 1000 children from Delhi’s poorest
+      communities, and your support will help them attend classes, engage in
+      learning and achieve.
     </Typography>
   );
 }
 
 function TEXT2() {
   return (
-    <Typography component="p" variant="body1" className="text1">
-      Towards this, we closely partner with national, state, and local
-      governments to design and implement high-quality, scalable solutions that
-      create these high-performing teachers, through a combination of training,
-      coaching and performance management.
+    <Typography
+      style={{ fontFamily: "Open Sans", fontWeight: 600 }}
+      component="p"
+      variant="body1"
+      className="text1"
+    >
+      For direct donations via Amazon Pay, please use the form below. To donate
+      a device/accessory, please see our{" "}
+      <a
+        href="https://www.amazon.in/hz/wishlist/ls/25X29BDNI7EQ1?&sort=default"
+        target="_blank"
+      >
+        <u>Amazon Wishlist</u>
+      </a>
+      . To know more about our work, please see our{" "}
+      <a href="https://www.peepulindia.org" target="_blank">
+        <u>website</u>
+      </a>
+      .
     </Typography>
   );
 }
 
-export { NAME, LOGO, HOME_PAGE, RETURN_URL, BLOG, TEXT1, TEXT2, TEXTHEAD };
+function DONATIONIMAGE() {
+  return (
+    <img style={{ height: "300px" }} src={DonationImage} alt="Donate"></img>
+  );
+}
+
+export {
+  NAME,
+  LOGO,
+  HOME_PAGE,
+  RETURN_URL,
+  BLOG,
+  TEXT1,
+  TEXT2,
+  TEXTHEAD,
+  DONATIONIMAGE,
+};
--- src/UIComponents/index.js
+++ src/UIComponents/index.js
@@ -1,13 +1,77 @@
-const NAME = "Navgurukul";
-const LOGO = "https://navgurukul.org/assets/img/logo.png";
-const HOME_PAGE = "https://navgurukul.org/";
-const RETURN_URL = "https://donate.navgurukul.org/";
-const BLOG = "https://medium.com/navgurukul";
-const TEXT1 = `More than 100+ students have been placed into various MNCs and startups as Software Programmers.`;
-const TEXT2 = `You can donate heartily to this campaign to support employment of girls from marginalised areas across the country.`;
-const TEXTHEAD = `Help girls from Low-Income Communities to become Software Engineers and uplift more women`;
+import React from "react";
+import { Typography } from "@material-ui/core";
 
-module.exports = {
+import DonationImage from "../assets/donation.jpg";
+
+const NAME = "Peepul";
+const LOGO = "https://navgurukul.org/assets/img/peepul.png";
+const HOME_PAGE = "https://www.peepulindia.org/";
+const RETURN_URL = "https://donate.peepulschools.org/";
+const BLOG = "https://twitter.com/peepulindia?lang=en";
+
+function TEXTHEAD() {
+  return (
+    <Typography
+      component="h4"
+      variant="h4"
+      className="Heading"
+      className="text-align-center"
+      style={{ fontFamily: "Comfortaa", color: "#262671", textAlign: "left" }}
+    >
+      Donate for devices.
+      <br />
+      Make a difference.
+    </Typography>
+  );
+}
+
+function TEXT1() {
+  return (
+    <Typography
+      style={{ fontFamily: "Open Sans", fontWeight: 600 }}
+      component="p"
+      variant="body1"
+      className="text1"
+    >
+      Thank you for your support! We support 1000 children from Delhi’s poorest
+      communities, and your support will help them attend classes, engage in
+      learning and achieve.
+    </Typography>
+  );
+}
+
+function TEXT2() {
+  return (
+    <Typography
+      style={{ fontFamily: "Open Sans", fontWeight: 600 }}
+      component="p"
+      variant="body1"
+      className="text1"
+    >
+      For direct donations via Amazon Pay, please use the form below. To donate
+      a device/accessory, please see our{" "}
+      <a
+        href="https://www.amazon.in/hz/wishlist/ls/25X29BDNI7EQ1?&sort=default"
+        target="_blank"
+      >
+        <u>Amazon Wishlist</u>
+      </a>
+      . To know more about our work, please see our{" "}
+      <a href="https://www.peepulindia.org" target="_blank">
+        <u>website</u>
+      </a>
+      .
+    </Typography>
+  );
+}
+
+function DONATIONIMAGE() {
+  return (
+    <img style={{ height: "300px" }} src={DonationImage} alt="Donate"></img>
+  );
+}
+
+export {
   NAME,
   LOGO,
   HOME_PAGE,
@@ -16,4 +80,5 @@ module.exports = {
   TEXT1,
   TEXT2,
   TEXTHEAD,
+  DONATIONIMAGE,
 };
