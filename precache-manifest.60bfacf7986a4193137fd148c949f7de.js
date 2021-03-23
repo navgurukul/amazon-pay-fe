a6aa2783544f2b2ce9b41501bd6b33fc43598c75
pysaquib <iamsaquibnasim@gmail.com>
2021-03-23 17:43:05 +0530

    gh-pages-pi update

---
+++ precache-manifest.60bfacf7986a4193137fd148c949f7de.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d67ecb9a82c97650e0ddefcafabc86a0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6987d75031ebe145c9c",
+    "url": "/static/css/main.3406939b.chunk.css"
+  },
+  {
+    "revision": "be39a1bdfeb31564b6f7",
+    "url": "/static/js/2.99393ae4.chunk.js"
+  },
+  {
+    "revision": "028d1b8f21ffff8caf0c799a051bd2b8",
+    "url": "/static/js/2.99393ae4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c615684b78f6bbd3d93e",
+    "url": "/static/js/3.7e4abf26.chunk.js"
+  },
+  {
+    "revision": "b6987d75031ebe145c9c",
+    "url": "/static/js/main.41a194a7.chunk.js"
+  },
+  {
+    "revision": "388dc5704860aa8df728",
+    "url": "/static/js/runtime-main.ff092066.js"
+  },
+  {
+    "revision": "93f5a5fd1193d804ffcfdbd54a19b125",
+    "url": "/static/media/donation.93f5a5fd.jpg"
+  },
+  {
+    "revision": "bd0ce86046aa79a181452878a8f66072",
+    "url": "/static/media/great_indian_sale.bd0ce860.jpg"
+  }
+]);
